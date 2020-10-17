160c1c7c683e46ec5e7b184a180b21224cb2d81b
Kiyotaka <t10kiyotaka@gmail.com>
2020-10-17 14:40:59 +0900

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,12 @@ fn hello_world() {
 }
 
 fn fizzbuzz(upto: u64) {
-    for i in 0..upto {
-        if i % 3 == 0 && i % 5 == 0 {
-            println!("FizzBuzz");
-        } else if i % 5 == 0 {
-            println!("Fizz");
-        } else if i % 3 == 0 {
-            println!("Buzz");
-        } else {
-            println!("{}", i);
+    for i in 1..upto {
+        match (i%3, i%5) {
+            (0, 0) => println!("FizzBuzz"),
+            (0, _) => println!("Fizz"),
+            (_, 0) => println!("Buzz"),
+            (_, _) => println!("{}", i)
         }
     }
 }
