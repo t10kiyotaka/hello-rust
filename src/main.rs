aa8d4f53eef03468b8333f64ce172eb76a4e4992
Kiyotaka <t10kiyotaka@gmail.com>
2020-10-17 13:56:11 +0900

    Add fizzbuzz

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,30 @@
 use ferris_says::say;
 use std::io::{stdout, BufWriter};
 
-fn main() {
+fn hello_world() {
     let stdout = stdout();
     let message = String::from("Hello fellow Rustaceans!");
     let width = message.chars().count();
 
     let mut writer = BufWriter::new(stdout.lock());
     say(message.as_bytes(), width, &mut writer).unwrap();
+}
+
+fn fizzbuzz(upto: u64) {
+    for i in 0..upto {
+        if i % 3 == 0 && i % 5 == 0 {
+            println!("FizzBuzz");
+        } else if i % 5 == 0 {
+            println!("Fizz");
+        } else if i % 3 == 0 {
+            println!("Buzz");
+        } else {
+            println!("{}", i);
+        }
+    }
+}
+
+fn main() {
+    hello_world();
+    fizzbuzz(30);
 }
